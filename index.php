80d8ed97d32213ae4dadc935d7156b2b2798460f
admin@calcoasttech.com <admin@la1.calcoasttech.com>
2015-03-19 14:41:57 -0700

    Reorganized the code in index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+/////////////////////////////////////////////
+// Preparation
+/////////////////////////////////////////////
+
 // Get RSS Eye Library (Repo: shttps://github.com/bryankaraffa/rss_eye)
 include_once('./includes/rss_eye/rss_eye.php');
 
@@ -9,13 +14,27 @@ include_once('./includes/rss_eye/rss_eye.php');
 //    $mysql_host = MySQL Hostname
 include_once('./config.php');
 
+// Get the rest of our required/predefined functions
 include_once('./functions.php');
 
+// Create an array to hold the new Items in
 $newItems = Array();
 
+// Check for CLI inputs and set those variables accordingly
+if (isset($argv)) {
+  foreach ($argv as $arg) {
+    switch ($arg) {
+      case '--disable-output':
+          $disable_output = true;
+          break;
+   }
+  }
+}
+
 /////////////////////////////////////////////
-// Site Scripts
+// Site-Specific Extraction and Filter Logic
 /////////////////////////////////////////////
+
 // reddit.com/r/Jeep
 $r_jeep = new rss_eye('http://www.reddit.com/r/Jeep/new.rss', './tmp/');
 foreach ($r_jeep->items['new'] as $item) {
@@ -32,25 +51,21 @@ foreach ($fiftycampfires->items['new'] as $item) {
   }
 }
 
-//
 
-if (isset($argv)) {
-  foreach ($argv as $arg) {
-    switch ($arg) {
-      case '--disable-output':
-          $disable_output = true;
-          break;
-   }
-  }
-}
-/* attempt connection */
+
+/////////////////////////////////////////////
+// Result Operations
+/////////////////////////////////////////////
+
+/////
+/* attempt mysql connection */
 $link = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
-/* check connection */
+/* check mysql connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
+/////
 
 // Insert New Results
 if (count($newItems) > 0) {
@@ -66,6 +81,7 @@ if (!isset($disable_output) || $disable_output != true) {
   printItems($items);
 }
 
+/////
 /* close connection */
 mysqli_close($link);
 
